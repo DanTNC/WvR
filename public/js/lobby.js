4130b1811ac34c24ae5cb7f242927fade423e46a
Ting An Yen <fad11204@yahoo.com.tw>
2018-05-07 14:28:16 +0000

    [Chore] remove dice.js

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -5,18 +5,18 @@ var R;
 $(document).ready(function(){
     
     var option = {
-    	speed : 10,
-    	duration : 3,
-    	stopImageNumber : -1,
-    	startCallback : function() {
-    		console.log('start');
-    	},
-    	slowDownCallback : function() {
-    		console.log('slowDown');
-    	},
-    	stopCallback : function($stopElm) {
-    		console.log('stop', $($stopElm).attr("id").replace('r',''));
-    	}
+        speed : 10,
+        duration : 3,
+        stopImageNumber : -1,
+        startCallback : function() {
+            console.log('start');
+        },
+        slowDownCallback : function() {
+            console.log('slowDown');
+        },
+        stopCallback : function($stopElm) {
+            console.log('stop', $($stopElm).attr("id").replace('r',''));
+        }
     };
     
     R = $('div.roulette').roulette(option);
